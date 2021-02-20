0d18a03e288a6c174352bb5b009da53bd8ae4411
Orlando Brown <iam@orlandobrown.uk>
2021-02-20 15:57:18 +0000

    chore: log params for rewrites

--- home/next.config.js
+++ home/next.config.js
@@ -1,7 +1,13 @@
 const { BLOG_URL } = process.env
 
 module.exports = {
-  rewrites() {
+  rewrites(a, b, c, d) {
+
+    console.log('param a: ', a)
+    console.log('param b: ', b)
+    console.log('param c: ', c)
+    console.log('param d: ', d)
+
     return [
       {
         source: '/blog',
